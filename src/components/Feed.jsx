b53e520e0231553d572d1967d945841743cdaaf1
Chaya <chaya.karim.omar@gmail.com>
2024-06-11 10:36:58 -0300

    v1.0

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,22 +1,40 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material'
+
+import { fetchFromAPI } from '../utilities/fetchFromAPI';
 import { SideBar, Videos } from '.';
 
 const Feed = () => {
+  
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => {
+          console.log(data)
+          setVideos(data.items)
+        }
+      )
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row"}}}>
       <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}>
-        <SideBar /> 
-        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
-          Copyright 2022 Youtube 2.0
+        <SideBar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        /> 
+        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff', opacity: 0.7 }}>
+          Copyright 2024 Youtube 2.0
         </Typography>
       </Box>
 
       <Box p={2} sx={{ overflowY: 'auto', height:'90vh', flex: 2}}>
         <Typography variant='h4' fontWeight="bold" mb={2} sx= {{ color:'white' }}>
-          New <span style={{ color:'#F31503'}}>videos</span>
+          {selectedCategory}<span style={{ color:'#F31503'}}> Videos</span>
         </Typography>
-        <Videos videos={[]}/>
+        <Videos videos={videos}/>
       </Box>
     </Stack>
   )
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Stack, Box } from '@mui/material'
+import { VideoCard, ChannelCard } from './'
+import { useState, useEffect } from 'react'
 
-const Videos = () => {
+const Videos = ({ videos, direction }) => {
+  const [videosFiltered, setVideosFiltered] = useState([]);
+
+  useEffect(()=> {
+    setVideosFiltered(videos.filter((item) => (item.id.videoId || item.id.channelId) && !item.id.playlistItem));
+  }, [videos])
+
+
+  if(!videos?.length) return "Loading...";
+  if(!videosFiltered?.length) return "Loading...";
+
   return (
-    <div>Videos</div>
+    <Stack direction={direction || "row" } flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videosFiltered.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item}/>}
+          {item.id.channelId && <ChannelCard channelDetail={item}/>}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
