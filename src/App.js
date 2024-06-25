d459a3794a5ae54720f6de0e1812c1e3be1b8d33
Chaya <chaya.karim.omar@gmail.com>
2024-06-25 19:18:50 -0300

    v 1.0.1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Box } from '@mui/material'
 import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components/index';
 
 const App = () => (
-    <BrowserRouter>
+    <BrowserRouter basename='/Youtube-2'>
       <Box sx={{ backgroundColor: '#000'}}> 
         <Navbar />
         <Routes>
